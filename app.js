f90ba3d125b96e4db8cfa1430b4ece060d67f38a
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2023-12-09 18:32:45 +0500

    Email API is successfully deployed on Vercel on 09-12-2023

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
-
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
